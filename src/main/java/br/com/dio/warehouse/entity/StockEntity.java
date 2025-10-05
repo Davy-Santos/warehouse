bed4da09c8a207f52491218238f58c8083eb7f8b
Davy-Santos <davy.santos06@gmail.com>
2025-10-05 14:11:39 -0300

    Microsserviço warehouse

---
+++ src/main/java/br/com/dio/warehouse/entity/StockEntity.java
@@ -0,0 +1,76 @@
+package br.com.dio.warehouse.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+import static br.com.dio.warehouse.entity.StockStatus.UNAVAILABLE;
+import static jakarta.persistence.EnumType.STRING;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class StockEntity {
+
+    @Id
+    private UUID id;
+
+    private Long amount;
+
+    private BigDecimal boughtPrice;
+
+    @Enumerated(STRING)
+    private StockStatus status;
+
+    private BigDecimal soldPrice;
+
+    public void decAmount(){
+        this.amount -= 1;
+        if (this.amount == 0) {
+            this.status = UNAVAILABLE;
+            
+        }
+    }
+
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private ProductEntity product;
+
+    public boolean isUnavailable(){
+        return status == UNAVAILABLE;
+    }
+
+
+    @Override
+    public boolean equals(final Object o) {
+        if (!(o instanceof final StockEntity that)) return false;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(amount, that.amount) &&
+                Objects.equals(boughtPrice, that.boughtPrice) &&
+                status == that.status &&
+                Objects.equals(soldPrice, that.soldPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, amount, boughtPrice, status, soldPrice);
+    }
+
+    @PrePersist
+    private void prePersist(){
+        this.id = UUID.randomUUID();
+    }
+}
